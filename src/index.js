745cbfcae94aabd57fbb77cb51da4e9929d3c814
Savitar2505 <us_vladislav@mail.ru>
2022-01-26 01:54:51 +0600

    done 1st react hm

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+const TodoSearch = () => {
+  return(
+      <div>
+          <input type="text"/>
+      </div>
+  )
+}
+
+const TodoStatusFilter = () => {
+  return(
+      <div>
+          <button>All</button>
+          <button>Active</button>
+          <button>Done</button>
+      </div>
+  )
+}
+
+const TodoHeader = () => {
+  return (
+      <div>
+          <TodoSearch />
+          <TodoStatusFilter/>
+      </div>
+  )
+}
+const TodoListItem = ({id, label, done, important}) => {
+  return (
+      <span>{label}</span>
+  )
+}
+
+const TodoList = ({todos}) => {
+
+    const elements = todos.map((item)=>{
+        return <li key={item.id}><TodoListItem {...item} /></li>
+    })
+    return(
+      <div>
+          <ul>{elements}</ul>
+      </div>
+    )
+}
+
+const App = () => {
+
+    const todos = [
+        {id: 1, label:'drink tea', done: false, important:false},
+        {id: 2, label:'drink coffee', done: false, important:false},
+        {id: 3, label:'learn react', done: false, important:false},
+        {id: 4, label:'drink vodka', done: false, important:false}
+    ]
+
+    return(
+        <div>
+            <TodoHeader />
+            <TodoList todos={todos}/>
+        </div>
+    )
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
